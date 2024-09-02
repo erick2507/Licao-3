81e8840f205f0325658a653ef10be3be204a5726
erick2507 <169068468+erick2507@users.noreply.github.com>
2024-09-02 12:17:31 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+
+Scanner sc = new Scanner(system.in);
+ Sytem.out.println("")   
+  }
+
+
+
+
+sc.close();
+  
+}
